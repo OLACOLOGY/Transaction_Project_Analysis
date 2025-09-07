e536f7b3b9efef0f0f3db8640e4e8636f36b5f27
Adetunji Olaoluwa <olaoluwachibuzor@1999>
2025-09-07 16:17:57 +0100

    new commit

--- task_done/project_sql/3_top_brand.sql
+++ task_done/project_sql/3_top_brand.sql
@@ -1,16 +1,51 @@
 --TOTAL SASLES BY BRAND
 -- This SQL script retriveves the top brands based on the total sales across diffrent cities.
-SELECT *,
+SELECT
     brand,
+    city,
     COUNT(quantity),
     ROUND(AVG(total_sales), 2) AS average_sales,
     SUM(total_sales) AS total_revenue
 FROM transactions_dim AS td
 LEFT JOIN product_name_dim AS pnd ON td.product_name = pnd.product_name
-GROUP BY brand
-ORDER BY total_revenue DESC;
+GROUP BY brand, city
+ORDER BY brand DESC;
 
 -- TOP SELLING BRAND BY CITY
 -- This SQL script retrieves the top-selling brand in specific cities: Abuja, Lagos, and Port Harcourt.
 
--- For 
+-- For Abuja
+SELECT
+    brand,
+    COUNT(quantity),
+    ROUND(AVG(total_sales), 2) AS average_sales,
+    SUM(total_sales) AS total_revenue
+    FROM transactions_dim AS td
+    LEFT JOIN product_name_dim AS pnd ON td.product_name = pnd.product_name
+    WHERE city = 'Abuja'
+    GROUP BY brand, city
+    ORDER BY total_revenue DESC;
+
+-- For Lagos
+SELECT
+    brand,
+    COUNT(quantity),
+    ROUND(AVG(total_sales), 2) AS average_sales,
+    SUM(total_sales) AS total_revenue
+    FROM transactions_dim AS td
+    LEFT JOIN product_name_dim AS pnd ON td.product_name = pnd.product_name
+    WHERE city = 'Lagos'
+    GROUP BY brand, city
+    ORDER BY total_revenue DESC;
+
+    -- For Port Harcourt
+SELECT
+    brand,
+    COUNT(quantity),
+    ROUND(AVG(total_sales), 2) AS average_sales,
+    SUM(total_sales) AS total_revenue
+    FROM transactions_dim AS td
+    LEFT JOIN product_name_dim AS pnd ON td.product_name = pnd.product_name
+    WHERE city = 'Port Harcourt'
+    GROUP BY brand, city
+    ORDER BY total_revenue DESC;
